c17b7925dc4d807e28daae6df10d05159db63027
Nguyen Anh Dao <dao.nguyen@smoovapp.com>
2016-05-27 02:52:58 +0700

    step 3.3

--- client/app.ts
+++ client/app.ts
@@ -1,12 +1,20 @@
+
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
-import { bootstrap } from '@angular/platform-browser-dynamic';
- 
+import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { Parties }   from '../collections/parties';
+import { Mongo }     from 'meteor/mongo';
+
 @Component({
-  selector: 'app',
+ selector: 'app',
   templateUrl: 'client/app.html'
 })
-class Socially { }
+class Socially {
+  parties: Mongo.Cursor<Object>;
+  constructor() {
+    this.parties = Parties.find();
+  }
+}
  
 bootstrap(Socially);
